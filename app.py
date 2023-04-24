e2fb8523195a79fdf78587dbae347beb80636806
Shetan <shetan.sisodia@metacube.com>
2023-04-24 15:07:44 +0530

    modification 29

--- app.py
+++ app.py
@@ -2,4 +2,4 @@
 import uvicorn
 from server import app
 if __name__ == "__main__":
-    uvicorn.run("server:app", host="127.0.0.1", port=8001, reload=False)
+    uvicorn.run("server:app", host="127.0.0.1", port=8001, reload=True)
